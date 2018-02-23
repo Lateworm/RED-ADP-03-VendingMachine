8f3c7f051545256a96490d2d2ee89d7bd57846cb
Late Worm <travis.rempel@gmail.com>
2018-02-23 13:51:21 -0800

    Improve makePayment test and function.

--- __tests__/makePayment.spec.js
+++ __tests__/makePayment.spec.js
@@ -4,7 +4,7 @@ describe('make change', () => {
   describe('when given a coin', () => {
     test('should return the inserted coin and the new total payment', () => {
       // each time a coin is inserted into the machine's coin slot, increment thepayment made
-      expect(machine.makePayment(1)).toEqual([1, 1]);
+      expect(machine.makePayment(0.05)).toEqual([0.05, 0.05]);
     });
   });
 });
