cd1bd9e7d08b55d0c9f9548430fd00f2ddc77b4e
Late Worm <travis.rempel@gmail.com>
2018-02-23 15:27:38 -0800

    Add a basic reportInventory test and function.

--- __tests__/reportInventory.spec.js
+++ __tests__/reportInventory.spec.js
@@ -1,9 +1,14 @@
-const machine = require('../lib/machine.js');
+const VendingMachine = require('../lib/machine.js');
+const productInventory = require('../__mocks__/stateInventoryFull');
+const coinInventory = require('../__mocks__/stateCoinFull');
+const payment = require('../__mocks__/stateCoinTwoFiddy');
+
+const machine = new VendingMachine(productInventory, coinInventory, payment);
 
 describe('report product inventory', () => {
   describe('when a complete product inventory report is requested', () => {
     test('should return the current stock of all products', () => {
-      expect(machine.reportInventory()).toEqual({});
+      expect(machine.reportInventory()).toEqual(productInventory);
     });
   });
 });
