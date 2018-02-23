d2235e7f4fd5cab964bff7799e6e39546b638e6d
Late Worm <travis.rempel@gmail.com>
2018-02-23 11:13:29 -0800

    Add a basic makePayment test and function.

--- __tests__/makePayment.spec.js
+++ __tests__/makePayment.spec.js
@@ -2,8 +2,9 @@ const machine = require('../lib/machine.js');
 
 describe('make change', () => {
   describe('when given a coin', () => {
-    test('should return the inserted coin, and the new total payment', () => {
-      expect(machine.makePayment('coin')).toEqual('coin');
+    test('should return the inserted coin and the new total payment', () => {
+      // each time a coin is inserted into the machine's coin slot, increment thepayment made
+      expect(machine.makePayment(1)).toEqual([1, 1]);
     });
   });
 });
