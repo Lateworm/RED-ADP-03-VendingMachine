466be799dab24fad6b76bc65ab50c0ca615bc780
Late Worm <travis.rempel@gmail.com>
2018-02-23 20:39:49 -0800

    Add a restockInventory test and function.

---
+++ __mocks__/stateInventoryEmpty.js
@@ -0,0 +1,42 @@
+module.exports = {
+  slot01: {
+    name: 'Cola',
+    stock: 0,
+    price: 1.75,
+  },
+  slot02: {
+    name: 'Iced Tea',
+    stock: 0,
+    price: 1.75,
+  },
+  slot03: {
+    name: 'Crisps',
+    stock: 0,
+    price: 2.25,
+  },
+  slot04: {
+    name: 'Cheeziez',
+    stock: 0,
+    price: 2.5,
+  },
+  slot05: {
+    name: 'Vegan Gluten-Free Low-Carb Bullshit',
+    stock: 0,
+    price: 4.25,
+  },
+  slot06: {
+    name: 'Milk Chocolate Candy Bar',
+    stock: 0,
+    price: 1.5,
+  },
+  slot07: {
+    name: 'Nougat Candy Bar',
+    stock: 0,
+    price: 2,
+  },
+  slot08: {
+    name: 'Peanut Butter Candy Bar',
+    stock: 0,
+    price: 2,
+  },
+};
--- __tests__/restockInventory.spec.js
+++ __tests__/restockInventory.spec.js
@@ -1,10 +1,16 @@
-const machine = require('../lib/machine.js');
-const restockQuantities = require('../__mocks__/restockInventory');
+const VendingMachine = require('../lib/machine.js');
+const productInventory = require('../__mocks__/stateInventoryEmpty');
+const coinInventory = require('../__mocks__/stateCoinFull');
+const payment = require('../__mocks__/stateCoinEmpty');
+
+const restockQuantities = require('../__mocks__/stateInventoryFull');
+
+const machine = new VendingMachine(productInventory, coinInventory, payment);
 
 describe('restock product inventory', () => {
   describe('when given an object describing product restock quantities', () => {
     test('should return an object describing the new product inventory', () => {
-      expect(machine.restockInventory(restockQuantities, machine.inventory)).toEqual({});
+      expect(machine.restockInventory(restockQuantities)).toEqual(restockQuantities);
     });
   });
 });
