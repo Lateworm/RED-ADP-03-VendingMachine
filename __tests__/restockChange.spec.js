030b8bfe2e65f88614cccb00ef940cbc9e73acc0
Late Worm <travis.rempel@gmail.com>
2018-02-23 20:04:25 -0800

    Add a restockChange test and function.

--- __tests__/restockChange.spec.js
+++ __tests__/restockChange.spec.js
@@ -1,10 +1,16 @@
-const machine = require('../lib/machine.js');
-const restockQuantities = require('../__mocks__/restockChange');
+const VendingMachine = require('../lib/machine.js');
+const productInventory = require('../__mocks__/stateInventoryFull');
+const coinInventory = require('../__mocks__/stateCoinEmpty');
+const payment = require('../__mocks__/stateCoinTwoFiddy');
+
+const restockQuantities = require('../__mocks__/stateCoinFull');
+
+const machine = new VendingMachine(productInventory, coinInventory, payment);
 
 describe('restock coin inventory', () => {
   describe('when given an object describing coin restock quantities', () => {
     test('should return an object describing the new coin inventory', () => {
-      expect(machine.restockChange(restockQuantities, machine.change)).toEqual({});
+      expect(machine.restockChange(restockQuantities)).toEqual(restockQuantities);
     });
   });
 });
