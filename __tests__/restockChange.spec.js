45e760146a4103f61d6fead1c673607a326a377b
Late Worm <travis.rempel@gmail.com>
2018-02-24 21:50:34 -0800

    Expand restockChange test suite.

--- __tests__/restockChange.spec.js
+++ __tests__/restockChange.spec.js
@@ -1,16 +1,42 @@
 const VendingMachine = require('../lib/machine.js');
 const productInventory = require('../__mocks__/stateInventoryFull');
-const coinInventory = require('../__mocks__/stateCoinEmpty');
+const fullCoinInventory = require('../__mocks__/stateCoinFull');
+const emptyCoinInventory = require('../__mocks__/stateCoinEmpty');
 const payment = require('../__mocks__/stateCoinTwoFiddy');
 
 const restockQuantities = require('../__mocks__/stateCoinFull');
 
-const machine = new VendingMachine(productInventory, coinInventory, payment);
+const fullMachine = new VendingMachine(productInventory, fullCoinInventory, payment);
+const emptyMachine = new VendingMachine(productInventory, emptyCoinInventory, payment);
 
 describe('restock coin inventory', () => {
-  describe('when given an object describing coin restock quantities', () => {
+  describe('when an empty machine is given a fresh float', () => {
     test('should return an object describing the new coin inventory', () => {
-      expect(machine.restockChange(restockQuantities)).toEqual(restockQuantities);
+      expect(emptyMachine.restockChange(restockQuantities)).toEqual(restockQuantities);
+    });
+  });
+
+  describe('when given payment for a snack', () => {
+    test('should return an object describing the new coin inventory', () => {
+      expect(fullMachine.restockChange(payment)).toEqual({
+        2: { value: 2, stock: 51 },
+        1: { value: 1, stock: 50 },
+        0.25: { value: 0.25, stock: 122 },
+        0.1: { value: 0.1, stock: 50 },
+        0.05: { value: 0.05, stock: 40 },
+      });
+    });
+  });
+
+  describe('when given payment for a snack', () => {
+    test("should add the coins from the payment to the machine's coin inventory", () => {
+      expect(fullMachine.coinInventory).toEqual({
+        2: { value: 2, stock: 51 },
+        1: { value: 1, stock: 50 },
+        0.25: { value: 0.25, stock: 122 },
+        0.1: { value: 0.1, stock: 50 },
+        0.05: { value: 0.05, stock: 40 },
+      });
     });
   });
 });
